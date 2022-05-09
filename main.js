23c0c13b37c22da454790e81c45acfd048e95d87
Fabio Moro <fabio.moro2000@gmail.com>
2022-05-09 16:35:30 +0200

    seconda versione

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 bombs = [];
 let bool = false;
 let cont = 0;
+let userNumber;
 
 for(let i=0; i < 16; i++) {
-
     let number;
-
     do {
          number = Math.floor(Math.random() * 100) + 1;
     }
@@ -14,30 +13,40 @@ for(let i=0; i < 16; i++) {
     bombs[i] = number;
 }
 
-let userNumbers = [];
+const container = document.querySelector(".container");
 
-for(let i=0; i < 84 && bool == false; i++) {
+for (let i=1; i <= 100; i++) {
 
-    let userNumber;
+    let element = document.createElement("div");
+    element.classList.add("square");
+    element.innerText = i;
+    container.append(element);
 
-    do {
-        userNumber = Number(prompt("inserisci un numero"));
-    }
-    while(userNumbers.includes(userNumber))
+    element.addEventListener('click',
 
-    userNumbers.push(userNumber);
+        function() {
+            userNumber = Number(element.innerText);
 
-    if(bombs.includes(userNumber)) {
-        bool = true;
-    } else {
-        cont = cont + 1;
-    }
+            if (bombs.includes(userNumber)) {
+                endGame();  
+            } 
+            else {
+                element.classList.add("safeNumber");
+                element.addEventListener('click',
+                    function() {
+                        alert("hai già scelto questo numero");
+                    }
+                )
+                cont = cont + 1;
+            }
+        }
+    )
 }
 
-if(bool) {
-    alert("hai perso!");+
-    alert("il tuo punteggio è "+cont);
-} else {
-    alert("hai vinto!");
+function endGame() {
+    let element = document.createElement("h1");
+    element.innerText = "Hai perso, il tuo punteggio è "+cont;
+    container.innerHTML="";
+    container.classList.add("finalContainer");
+    container.append(element);
 }
-
