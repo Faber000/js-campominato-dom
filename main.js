91e2550444dd325ac07b75fb84b0cd15f42c87ee
Fabio Moro <fabio.moro2000@gmail.com>
2022-05-12 15:25:49 +0200

    prima versione

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ for (let i=1; i <= 100; i++) {
                     }
                 )
                 cont = cont + 1;
-                if (cont==84) {
+                if (cont==5) {
                     winGame();
                 }
             }
